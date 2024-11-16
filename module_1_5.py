66157e0316481e7d9d3f103df24aab9b4ee0198c
slike29 <craftcubs123@gmail.com>
2024-11-16 12:06:10 +1000

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,10 @@
+immutable_var = (1, 1.5, "Work", False)
+print(immutable_var)
+
+# immutable_var[0] = "Clock"
+
+mutable_list = [41, True, "Wood"]
+mutable_list[0] = "Urban"
+mutable_list[1] = False
+mutable_list[2] = 2024
+print(mutable_list)
